2a466a089358f88cb7d1f11537e84fb2671da1a4
taiyuejie <1106864467@qq.com>
2020-02-25 22:55:33 +0800

    add merge sort

---
+++ src/main/java/com/tiger/sort/Merge.java
@@ -0,0 +1,79 @@
+package com.tiger.sort;
+
+import java.util.Arrays;
+
+/**
+ * @auther Jack
+ * @date 2020/2/25 21:47
+ */
+
+public class Merge<T extends Comparable<T>> extends Sort<T> {
+
+    private T[] aux;
+
+    @Override
+    public void sort(T[] arr) {
+
+        aux = (T[]) new Comparable[arr.length];
+
+        mergeBU(arr);
+
+    }
+
+    public void sort(T[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + (r - l) / 2;
+
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private void merge(T[] arr, int l, int mid, int r) {
+
+        if (l == r) {
+            return;
+        }
+
+        for (int i = l; i <= r; i++) {
+            aux[i] = arr[i];
+        }
+
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+
+            if (i > mid) {
+                arr[k] = aux[j++];
+            } else if (j > r) {
+                arr[k] = aux[i++];
+            } else if (less(aux[j], aux[k])) {
+                arr[k] = aux[j++];
+            } else {
+                arr[k] = aux[i++];
+            }
+        }
+    }
+
+    private void mergeBU(T[] arr) {
+
+        int n = arr.length;
+
+        for (int sz = 1; sz < n; sz += sz) {
+            for (int i = 0; i + sz < n; i += sz + sz) {
+                //arr[i,i+sz-1],arr[i+sz,i+sz+sz-1]
+                merge(arr, i, i + sz - 1, Math.min(i + sz + sz - 1, n - 1));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = new Integer[]{4, 3, 2, 1, 5};
+
+        new Merge<Integer>().sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
