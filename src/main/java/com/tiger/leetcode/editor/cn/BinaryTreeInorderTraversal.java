70118d4bf473de9a0b29c4391d5fe6b82378ee69
taiyuejie <1106864467@qq.com>
2020-04-26 22:35:46 +0800

    add lc 94

--- src/main/java/com/tiger/leetcode/editor/cn/BinaryTreeInorderTraversal.java
+++ src/main/java/com/tiger/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -18,41 +18,75 @@
 package com.tiger.leetcode.editor.cn;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class BinaryTreeInorderTraversal {
-    public static void main(String[] args) {
-        Solution solution = new BinaryTreeInorderTraversal().new Solution();
-    }
-    //leetcode submit region begin(Prohibit modification and deletion)
+	public static void main(String[] args) {
+		Solution solution = new BinaryTreeInorderTraversal().new Solution();
+	}
+	//leetcode submit region begin(Prohibit modification and deletion)
 
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     * int val;
-     * TreeNode left;
-     * TreeNode right;
-     * TreeNode(int x) { val = x; }
-     * }
-     */
-    class Solution {
-        List<Integer> inorderNums = new ArrayList<Integer>();
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 * int val;
+	 * TreeNode left;
+	 * TreeNode right;
+	 * TreeNode(int x) { val = x; }
+	 * }
+	 */
+	class Solution {
+		List<Integer> inorderNums = new ArrayList<>();
 
-        public List<Integer> inorderTraversal(TreeNode root) {
-            inorderTraversalRecursive(root);
-            return inorderNums;
-        }
+		class PathNode {
+			TreeNode node;
+			Boolean visited;
 
-        public void inorderTraversalRecursive(TreeNode root) {
-            if (root == null) {
-                return;
-            }
-            inorderTraversalRecursive(root.left);
-            inorderNums.add(root.val);
-            inorderTraversalRecursive(root.right);
-        }
-    }
+			public PathNode(TreeNode node, Boolean visited) {
+				this.node = node;
+				this.visited = visited;
+			}
+		}
+
+
+		public List<Integer> inorderTraversal(TreeNode root) {
+			List<Integer> res = new LinkedList<>();
+			if (root == null) return res;
+			Stack<PathNode> st = new Stack<>();
+			st.add(new PathNode(root, false));
+			while (!st.isEmpty()) {
+				PathNode pathNode = st.pop();
+				if (pathNode.visited) {
+					res.add(pathNode.node.val);
+				} else {
+					if (pathNode.node.right != null) {
+						st.add(new PathNode(pathNode.node.right, false));
+					}
+					st.add(new PathNode(pathNode.node, true));
+					if (pathNode.node.left != null) {
+						st.add(new PathNode(pathNode.node.left, false));
+					}
+				}
+			}
+			return res;
+		}
+
+		public List<Integer> inorderTraversalV1(TreeNode root) {
+			inorderTraversalRecursive(root);
+			return inorderNums;
+		}
+
+		public void inorderTraversalRecursive(TreeNode root) {
+			if (root == null) {
+				return;
+			}
+			inorderTraversalRecursive(root.left);
+			inorderNums.add(root.val);
+			inorderTraversalRecursive(root.right);
+		}
+	}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
