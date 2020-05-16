075847e736834f48f23bd027d9afa29cd563b45a
taiyuejie <1106864467@qq.com>
2020-05-16 15:58:03 +0800

    二刷17:电话号码的字母组合

--- src/main/java/com/tiger/leetcode/editor/cn/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/tiger/leetcode/editor/cn/LetterCombinationsOfAPhoneNumber.java
@@ -23,54 +23,76 @@ import java.util.List;
 import java.util.Map;
 
 public class LetterCombinationsOfAPhoneNumber {
-    public static void main(String[] args) {
-        Solution solution = new LetterCombinationsOfAPhoneNumber().new Solution();
-        List<String> list = solution.letterCombinations("23");
-        System.out.println(list.toString());
-    }
+	public static void main(String[] args) {
+		Solution solution = new LetterCombinationsOfAPhoneNumber().new Solution();
+		List<String> list = solution.letterCombinations("23");
+		System.out.println(list.toString());
+	}
 
-    //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
+	//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		private List<String> res = new ArrayList<>();
+		private String[] phone = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        Map<Character, String> phone = new HashMap<Character, String>() {
-            {
-                put('2', "abc");
-                put('3', "def");
-                put('4', "ghi");
-                put('5', "jkl");
-                put('6', "mno");
-                put('7', "pqrs");
-                put('8', "tuv");
-                put('9', "wxyz");
-            }
-        };
+		public List<String> letterCombinations(String digits) {
+			if (digits == null || digits.isEmpty()) return res;
+			letterCombinations(digits, 0, "");
+			return res;
+		}
 
-        List<String> result = new ArrayList<>();
+		private void letterCombinations(String digits, int index, String path) {
+			if (index == digits.length()) {
+				res.add(path);
+				return;
+			}
+			String str = phone[digits.charAt(index) - '0'];
+			for (int i = 0; i < str.length(); i++) {
+				letterCombinations(digits, index + 1, path + str.substring(i, i + 1));
+			}
+		}
+	}
 
-        public List<String> letterCombinations(String digits) {
+	//leetcode submit region end(Prohibit modification and deletion)
+	class SolutionV1 {
 
-            if (digits == null || digits.isEmpty()) {
-                return result;
-            }
-            backTrack(digits, "");
+		Map<Character, String> phone = new HashMap<Character, String>() {
+			{
+				put('2', "abc");
+				put('3', "def");
+				put('4', "ghi");
+				put('5', "jkl");
+				put('6', "mno");
+				put('7', "pqrs");
+				put('8', "tuv");
+				put('9', "wxyz");
+			}
+		};
 
-            return result;
-        }
+		List<String> result = new ArrayList<>();
 
-        public void backTrack(String digits, String combinedLetter) {
+		public List<String> letterCombinations(String digits) {
 
-            if (digits.isEmpty()) {
-                result.add(combinedLetter);
-                return;
-            }
+			if (digits == null || digits.isEmpty()) {
+				return result;
+			}
+			backTrack(digits, "");
 
-            String letters = phone.get(digits.charAt(0));
-            for (int i = 0; i < letters.length(); i++) {
-                String letter = letters.substring(i, i + 1);
-                backTrack(digits.substring(1), combinedLetter + letter);
-            }
-        }
-    }
-//leetcode submit region end(Prohibit modification and deletion)
+			return result;
+		}
+
+		public void backTrack(String digits, String combinedLetter) {
+
+			if (digits.isEmpty()) {
+				result.add(combinedLetter);
+				return;
+			}
+
+			String letters = phone.get(digits.charAt(0));
+			for (int i = 0; i < letters.length(); i++) {
+				String letter = letters.substring(i, i + 1);
+				backTrack(digits.substring(1), combinedLetter + letter);
+			}
+		}
+	}
 
 }
