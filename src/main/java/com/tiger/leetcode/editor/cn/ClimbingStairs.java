df057074b82fe7cd4476df80f60b6efb4cf2152f
taiyuejie <1106864467@qq.com>
2020-06-06 09:20:56 +0800

    198.打家劫舍,跑楼梯v2

--- src/main/java/com/tiger/leetcode/editor/cn/ClimbingStairs.java
+++ src/main/java/com/tiger/leetcode/editor/cn/ClimbingStairs.java
@@ -32,8 +32,25 @@ public class ClimbingStairs {
 	}
 
 	//leetcode submit region begin(Prohibit modification and deletion)
-	//Date:2020-05-28 22:12:30
+	//执行耗时:0 ms,击败了100.00% 的Java用户
+	//Date:2020-06-06 09:19:49
 	class Solution {
+		public int climbStairs(int n) {
+			if (n <= 0) return 0;
+
+			int dp[] = new int[n + 1];
+			dp[n] = 1;
+			dp[n - 1] = 1;
+			for (int i = n - 2; i >= 0; i--) {
+				dp[i] = dp[i + 1] + dp[i + 2];
+			}
+			return dp[0];
+		}
+	}
+	//leetcode submit region end(Prohibit modification and deletion)
+
+	//Date:2020-05-28 22:12:30
+	class SolutionV1 {
 		public int climbStairs(int n) {
 			if (n < 3) return n;
 
@@ -46,6 +63,5 @@ public class ClimbingStairs {
 			return dp[n];
 		}
 	}
-//leetcode submit region end(Prohibit modification and deletion)
 
 }
