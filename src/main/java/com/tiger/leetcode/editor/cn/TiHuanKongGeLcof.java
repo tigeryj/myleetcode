26b406d940cbeab13e01b9980a260a05d665607d
taiyuejie <1106864467@qq.com>
2020-06-26 10:27:45 +0800

    剑指 Offer 05.替换空格

---
+++ src/main/java/com/tiger/leetcode/editor/cn/TiHuanKongGeLcof.java
@@ -0,0 +1,46 @@
+//剑指 Offer 05.替换空格
+//请实现一个函数，把字符串 s 中的每个空格替换成"%20"。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "We are happy."
+//输出："We%20are%20happy." 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= s 的长度 <= 10000 
+//
+
+package com.tiger.leetcode.editor.cn;
+
+public class TiHuanKongGeLcof {
+	public static void main(String[] args) {
+		Solution solution = new TiHuanKongGeLcof().new Solution();
+	}
+
+	//leetcode submit region begin(Prohibit modification and deletion)
+
+	//Date:2020-06-26 10:26:09
+	//解答成功: 执行耗时:0 ms,击败了100.00% 的Java用户
+	class Solution {
+		public String replaceSpace(String s) {
+			if (s == null) throw new IllegalArgumentException();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if (c == ' ') {
+					sb.append("%20");
+				} else {
+					sb.append(c);
+				}
+			}
+			return sb.toString();
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
