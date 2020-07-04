f2a69cb9adb6c72ec07a09e409796fbf9b47c050
taiyuejie <1106864467@qq.com>
2020-07-04 20:13:28 +0800

    【递归】剑指 Offer 26.树的子结构A和B，判断B是不是A的子结构

---
+++ src/main/java/com/tiger/leetcode/editor/cn/ShuDeZiJieGouLcof.java
@@ -0,0 +1,72 @@
+//剑指 Offer 26.树的子结构A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+//
+// B是A的子结构， 即 A中有出现和B相同的结构和节点值。 
+//
+// 例如: 
+//给定的树 A: 
+//
+// 3 
+// / \ 
+// 4 5 
+// / \ 
+// 1 2 
+//给定的树 B： 
+//
+// 4 
+// / 
+// 1 
+//返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。 
+//
+// 示例 1： 
+//
+// 输入：A = [1,2,3], B = [3,1]
+//输出：false
+// 
+//
+// 示例 2： 
+//
+// 输入：A = [3,4,5,1,2], B = [4,1]
+//输出：true 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 10000 
+// Related Topics 树
+
+package com.tiger.leetcode.editor.cn;
+
+public class ShuDeZiJieGouLcof {
+	public static void main(String[] args) {
+		Solution solution = new ShuDeZiJieGouLcof().new Solution();
+	}
+	//leetcode submit region begin(Prohibit modification and deletion)
+
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 * int val;
+	 * TreeNode left;
+	 * TreeNode right;
+	 * TreeNode(int x) { val = x; }
+	 * }
+	 */
+	//Date:2020-07-04 20:08:12
+	//解答成功: 执行耗时:0 ms,击败了100.00% 的Java用户
+	class Solution {
+		public boolean isSubStructure(TreeNode A, TreeNode B) {
+			if (B == null || A == null) return false;
+
+			return isSub(A, B) || isSubStructure(A.left, B) || isSub(A.right, B);
+		}
+
+		private boolean isSub(TreeNode A, TreeNode B) {
+			if (B == null) return true;
+			if (A == null) return false;
+
+			return A.val == B.val && isSub(A.left, B.left) && isSub(A.right, B.right);
+
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
