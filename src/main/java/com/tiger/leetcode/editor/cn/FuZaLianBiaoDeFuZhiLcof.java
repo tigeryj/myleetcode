808785ea6406218b1ef79ad2667d5ebde1ded2df
taiyuejie <1106864467@qq.com>
2020-07-09 23:36:47 +0800

    剑指 Offer 35.复杂链表的复制

---
+++ src/main/java/com/tiger/leetcode/editor/cn/FuZaLianBiaoDeFuZhiLcof.java
@@ -0,0 +1,138 @@
+//剑指 Offer 35.复杂链表的复制
+//请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指
+//向链表中的任意节点或者 null。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+//输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 输入：head = [[1,1],[2,1]]
+//输出：[[1,1],[2,1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 输入：head = [[3,null],[3,0],[3,null]]
+//输出：[[3,null],[3,0],[3,null]]
+// 
+//
+// 示例 4： 
+//
+// 输入：head = []
+//输出：[]
+//解释：给定的链表为空（空指针），因此返回 null。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -10000 <= Node.val <= 10000 
+// Node.random 为空（null）或指向链表中的节点。 
+// 节点数目不超过 1000 。 
+// 
+//
+// 
+//
+// 注意：本题与主站 138 题相同：https://leetcode-cn.com/problems/copy-list-with-random-point
+//er/ 
+//
+// 
+// Related Topics 链表
+
+package com.tiger.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FuZaLianBiaoDeFuZhiLcof {
+	public static void main(String[] args) {
+		Solution solution = new FuZaLianBiaoDeFuZhiLcof().new Solution();
+	}
+
+	//leetcode submit region begin(Prohibit modification and deletion)
+
+
+	//Date:2020-07-09 23:35:56
+	//执行耗时:0 ms,击败了100.00% 的Java用户
+	class Solution {
+		public Node copyRandomList(Node head) {
+			if (head == null) return null;
+			Node cur = head;
+			Map<Node, Node> map = new HashMap<>();
+			while (cur != null) {
+				map.put(cur, new Node(cur.val));
+				cur = cur.next;
+			}
+
+			cur = head;
+			while (cur != null) {
+				map.get(cur).next = map.get(cur.next);
+				map.get(cur).random = map.get(cur.random);
+				cur = cur.next;
+			}
+			return map.get(head);
+		}
+	}
+
+	//leetcode submit region end(Prohibit modification and deletion)
+	class Node {
+		int val;
+		Node next;
+		Node random;
+
+		public Node(int val) {
+			this.val = val;
+			this.next = null;
+			this.random = null;
+		}
+	}
+
+	//Date:2020-07-09 23:07:11
+	//解答成功: 执行耗时:1 ms,击败了15.94% 的Java用户
+	class SolutionV1 {
+		public Node copyRandomList(Node head) {
+			if (head == null) return null;
+			Node dummy = new Node(-1);
+			Node cur = dummy;
+			Node run = head;
+			HashMap<Node, Integer> map1 = new HashMap<>();
+			HashMap<Integer, Node> map2 = new HashMap<>();
+			int count = 0;
+			while (run != null) {
+				cur.next = new Node(run.val);
+				cur = cur.next;
+
+				map1.put(run, count);
+				map2.put(count, cur);
+				run = run.next;
+				count++;
+			}
+
+			run = head;
+			cur = dummy.next;
+			while (run != null) {
+				if (run.random != null) {
+					cur.random = map2.get(map1.get(run.random));
+				}
+				run = run.next;
+				cur = cur.next;
+			}
+			return dummy.next;
+		}
+	}
+
+}
